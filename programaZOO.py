a11c2b9756a2420f8e2f77d6f650ce440ffb22f2
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-08-22 23:31:48 +0000

    feito

---
+++ programaZOO.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self,nome,idade,barulho,movimento,alimentacao,habitat,vizinhos,horas_alimentacao):
+        self.nome = nome
+        self.idade = idade
+        self.barulho = barulho
+        self.movimento = movimento
+        self.alimentacao = alimentacao
+        self.habitat = habitat
+        self.vizinhos = []
+        self.horas_alimentacao = horas_alimentacao
